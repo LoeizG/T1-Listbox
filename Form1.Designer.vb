34ef2d04c16cd36ef49b3c1c7ddff88136bd5c36
Loeiz07 <loeiz.gonzalez@gmail.com>
2022-09-09 11:29:46 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.lbolist = New System.Windows.Forms.ListBox()
+        Me.lboECivil = New System.Windows.Forms.ListBox()
+        Me.lboTelefono = New System.Windows.Forms.ListBox()
+        Me.lboEdad = New System.Windows.Forms.ListBox()
+        Me.lboNombre = New System.Windows.Forms.ListBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lboIndice = New System.Windows.Forms.ListBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtIngresados = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(29, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Apellido y Nombre:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(29, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Edad:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(29, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Teléfono:"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(492, 146)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(74, 19)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Casado"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAgregar.Location = New System.Drawing.Point(613, 27)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAgregar.TabIndex = 4
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEliminar.Location = New System.Drawing.Point(613, 69)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 5
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.Location = New System.Drawing.Point(613, 107)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 6
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(613, 150)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 7
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(163, 35)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(299, 20)
+        Me.txtNombre.TabIndex = 8
+        '
+        'txtEdad
+        '
+        Me.txtEdad.Location = New System.Drawing.Point(163, 91)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(299, 20)
+        Me.txtEdad.TabIndex = 9
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(163, 145)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(299, 20)
+        Me.txtTelefono.TabIndex = 10
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.txtIngresados)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.lboIndice)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Controls.Add(Me.lbolist)
+        Me.Panel1.Controls.Add(Me.lboECivil)
+        Me.Panel1.Controls.Add(Me.lboTelefono)
+        Me.Panel1.Controls.Add(Me.lboEdad)
+        Me.Panel1.Controls.Add(Me.lboNombre)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Location = New System.Drawing.Point(32, 214)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(656, 210)
+        Me.Panel1.TabIndex = 11
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(-46, 52)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.ListBox1.Size = New System.Drawing.Size(40, 108)
+        Me.ListBox1.TabIndex = 19
+        '
+        'lbolist
+        '
+        Me.lbolist.BackColor = System.Drawing.Color.White
+        Me.lbolist.FormattingEnabled = True
+        Me.lbolist.Location = New System.Drawing.Point(-35, 52)
+        Me.lbolist.Name = "lbolist"
+        Me.lbolist.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.lbolist.Size = New System.Drawing.Size(29, 108)
+        Me.lbolist.TabIndex = 18
+        '
+        'lboECivil
+        '
+        Me.lboECivil.FormattingEnabled = True
+        Me.lboECivil.Location = New System.Drawing.Point(534, 51)
+        Me.lboECivil.Name = "lboECivil"
+        Me.lboECivil.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.lboECivil.Size = New System.Drawing.Size(102, 108)
+        Me.lboECivil.TabIndex = 17
+        '
+        'lboTelefono
+        '
+        Me.lboTelefono.FormattingEnabled = True
+        Me.lboTelefono.Location = New System.Drawing.Point(410, 51)
+        Me.lboTelefono.Name = "lboTelefono"
+        Me.lboTelefono.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.lboTelefono.Size = New System.Drawing.Size(102, 108)
+        Me.lboTelefono.TabIndex = 16
+        '
+        'lboEdad
+        '
+        Me.lboEdad.FormattingEnabled = True
+        Me.lboEdad.Location = New System.Drawing.Point(286, 51)
+        Me.lboEdad.Name = "lboEdad"
+        Me.lboEdad.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.lboEdad.Size = New System.Drawing.Size(102, 108)
+        Me.lboEdad.TabIndex = 15
+        '
+        'lboNombre
+        '
+        Me.lboNombre.FormattingEnabled = True
+        Me.lboNombre.Location = New System.Drawing.Point(69, 51)
+        Me.lboNombre.Name = "lboNombre"
+        Me.lboNombre.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.lboNombre.Size = New System.Drawing.Size(192, 108)
+        Me.lboNombre.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(544, 18)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(82, 15)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Estado Civil"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(423, 18)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(63, 15)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Teléfono"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(314, 18)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 15)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Edad"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(100, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(124, 15)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Apellido y Nombre"
+        '
+        'lboIndice
+        '
+        Me.lboIndice.FormattingEnabled = True
+        Me.lboIndice.Location = New System.Drawing.Point(24, 51)
+        Me.lboIndice.Name = "lboIndice"
+        Me.lboIndice.Size = New System.Drawing.Size(30, 108)
+        Me.lboIndice.TabIndex = 20
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(161, 180)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(114, 15)
+        Me.Label8.TabIndex = 21
+        Me.Label8.Text = "Total Ingresados"
+        '
+        'txtIngresados
+        '
+        Me.txtIngresados.Location = New System.Drawing.Point(286, 180)
+        Me.txtIngresados.Name = "txtIngresados"
+        Me.txtIngresados.ReadOnly = True
+        Me.txtIngresados.Size = New System.Drawing.Size(100, 20)
+        Me.txtIngresados.TabIndex = 22
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(723, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtEdad)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.Text = " Datos Generales"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtEdad As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lboNombre As ListBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents lbolist As ListBox
+    Friend WithEvents lboECivil As ListBox
+    Friend WithEvents lboTelefono As ListBox
+    Friend WithEvents lboEdad As ListBox
+    Friend WithEvents lboIndice As ListBox
+    Friend WithEvents txtIngresados As TextBox
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub BtnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        lboNombre.Items.Add(txtNombre.Text)
+        lboEdad.Items.Add(txtEdad.Text)
+        lboTelefono.Items.Add(txtTelefono.Text)
+        lboECivil.Items.Add(IIf(CheckBox1.Checked, "Casado", "Soltero"))
+        lboIndice.Items.Add(" " + lboNombre.Items.Count.ToString + " -")
+        txtIngresados.Text = lboIndice.Items.Count
+        txtNombre.Text = ""
+        txtEdad.Text = ""
+        txtTelefono.Text = ""
+        CheckBox1.CheckState = CheckState.Unchecked
+        txtNombre.Focus()
+    End Sub
+
+    Private Sub BtnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim i As Integer = lboIndice.SelectedIndex
+        lboNombre.Items.RemoveAt(i)
+        lboEdad.Items.RemoveAt(i)
+        lboTelefono.Items.RemoveAt(i)
+        lboECivil.Items.RemoveAt(i)
+        lboIndice.Items.RemoveAt(lboIndice.Items.Count - 1)
+        txtIngresados.Text = lboIndice.Items.Count
+    End Sub
+
+    Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        txtNombre.Text = ""
+        txtTelefono.Text = ""
+        txtEdad.Text = ""
+        CheckBox1.CheckState = CheckState.Unchecked
+    End Sub
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Me.Close()
+    End Sub
+
+
+End Class
